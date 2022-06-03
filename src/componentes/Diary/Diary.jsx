8384f108d8c6da1b28a63c5f98258c1f72d8019b
sebasY0725 <shebas0725@gmail.com>
2022-06-03 16:53:01 -0500

    Diary Css terminado

--- src/componentes/Diary/Diary.jsx
+++ src/componentes/Diary/Diary.jsx
@@ -1,6 +1,7 @@
 import "./diary.css";
 import { useState, useEffect } from "react";
-
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import MessageIcon from "@mui/icons-material/Message";
 export default function Diary() {
   const [data, setData] = useState({});
   const [loading, setLoading] = useState(true);
@@ -33,12 +34,77 @@ export default function Diary() {
 
   return (
     <div className="Diary">
-      Conexiones de agenda para hoy
-      <li>{data[1]?.name}</li>
-      <li>{data[2]?.name}</li>
-      <li>{data[3]?.name}</li>
-      <li>{data[4]?.name}</li>
-      <li>{data[5]?.name}</li>
+      <h2 className="DiaryTitle">Conexiones de agenda para hoy</h2>
+
+      <li className="ListItem">
+        <div className="ColorLine"></div>
+        <p className="ItemName">{data[1]?.name}</p>
+        <div className="ItemsLists">
+          <p className="ListItemP1">Llamar</p>
+          <p className="ListItemP2">Descuento temporada</p>
+        </div>
+        <p className="StatusIcon">🟠</p>
+        <div className="ListItemIcons">
+          <LocalPhoneIcon />
+          <MessageIcon />
+        </div>
+      </li>
+
+      <li className="ListItem ListItemWhite">
+        <div className="ColorLine ColorLine2"></div>
+        <p className="ItemName">{data[2]?.name}</p>
+        <div className="ItemsLists">
+          <p className="ListItemP1">Llamar</p>
+          <p className="ListItemP2">Descuento temporada</p>
+        </div>
+        <p className="StatusIcon">🟠</p>
+        <div className="ListItemIcons">
+          <LocalPhoneIcon />
+          <MessageIcon />
+        </div>
+      </li>
+
+      <li className="ListItem">
+        <div className="ColorLine ColorLine3"></div>
+        <p className="ItemName">{data[3]?.name}</p>
+        <div className="ItemsLists">
+          <p className="ListItemP1">Llamar</p>
+          <p className="ListItemP2">Descuento temporada</p>
+        </div>
+        <p className="StatusIcon">⚪</p>
+        <div className="ListItemIcons">
+          <LocalPhoneIcon />
+          <MessageIcon />
+        </div>
+      </li>
+
+      <li className="ListItem ListItemWhite">
+        <div className="ColorLine"></div>
+        <p className="ItemName">{data[4]?.name}</p>
+        <div className="ItemsLists">
+          <p className="ListItemP1">Llamar</p>
+          <p className="ListItemP2">Descuento temporada</p>
+        </div>
+        <p className="StatusIcon">🟠</p>
+        <div className="ListItemIcons">
+          <LocalPhoneIcon />
+          <MessageIcon />
+        </div>
+      </li>
+
+      <li className="ListItem">
+        <div className="ColorLine ColorLine3"></div>
+        <p className="ItemName">{data[5]?.name}</p>
+        <div className="ItemsLists">
+          <p className="ListItemP1">Llamar</p>
+          <p className="ListItemP2">Descuento temporada</p>
+        </div>
+        <p className="StatusIcon">🟠</p>
+        <div className="ListItemIcons">
+          <LocalPhoneIcon />
+          <MessageIcon />
+        </div>
+      </li>
     </div>
   );
 }
